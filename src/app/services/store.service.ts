2e72b6609a4dda2a712f4f6b703bcdaff0a24d6c
Lucas Busso <lucasxbusso@gmail.com>
2023-08-02 15:38:03 -0300

    store service. get all prod fetch method

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { IProduct } from "../models/product.model";
+import { environment } from "src/environments/environment";
 
+// const STORE_BASE_URL = 'https://fakestoreapi.com'
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class StoreService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getAllProducts(
+    limit: number = 12,
+    sort: string = "desc"
+  ): Observable<Array<IProduct>> {
+    return this.httpClient.get<Array<IProduct>>(
+      `${environment.STORE_BASE_URL}/products?sort=${sort}&limit=${limit}`
+    );
+  }
 }
