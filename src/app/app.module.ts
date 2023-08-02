489bbaa25792256afeb0a10bca67557cede0ba73
Lucas Busso <lucasxbusso@gmail.com>
2023-08-02 15:00:49 -0300

    store service generate and added to app providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { FiltersComponent } from "./pages/home/components/filters/filters.compon
 import { ProductTileComponent } from "./pages/home/components/product-tile/product-tile.component";
 import { CartComponent } from "./pages/cart/cart.component";
 import { CartService } from "./services/cart.service";
+import { StoreService } from "./services/store.service";
 
 @NgModule({
   declarations: [
@@ -52,7 +53,7 @@ import { CartService } from "./services/cart.service";
     MatBadgeModule,
     MatSnackBarModule,
   ],
-  providers: [CartService],
+  providers: [CartService, StoreService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
